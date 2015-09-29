a8560a64e25a98cbd16d34be8cd588e962c3c7e1
Tyler Kaminsky <tylerkaminsky@gmail.com>
2015-09-28 23:11:40 -0400

    working platform generation and movement

---
+++ Assets/Scripts/PlatformScript.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformScript : MonoBehaviour {
+
+	public GameObject platform;
+	public GameObject[] plats  = new GameObject[10];
+	//private ArrayList plats;
+
+
+	// Use this for initialization
+	void Start () {
+		float move = 0;
+		//platform = Instantiate(Resources.Load("Prefabs/Platforms"), new Vector2(move,move), Quaternion.identity) as GameObject;
+
+
+		//ArrayList plats = new ArrayList();
+		//plats = new (ArrayList)ArrayList[10];
+
+		for (int i = 0; i <= 9; i++) {
+
+			//plats = new GameObject[10];
+
+			GameObject clone = (GameObject)Instantiate (platform, new Vector3(move,move,0), Quaternion.identity);
+
+			print ("plats[i] " + plats[i]);
+			
+			print ("adding plat " + i);
+			print ("move " + move);
+
+			plats[i] = clone;
+			plats[i].transform.Translate(new Vector3(move,move,0));
+			move++;
+
+
+
+
+
+	
+		}
+	
+
+	}
+
+	//public Rigidbody2D platformPrefab;
+
+	// Update is called once per frame
+	void Update () {
+	
+		float move = .005f;
+		for (int i = 0; i <= 9; i++) {
+			plats[i].transform.Translate(new Vector3(move,0,0));
+		}
+	}
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class PlayerMovement : MonoBehaviour {
 
+	public float jumpForce;
 	// Use this for initialization
 	void Start () {
 	
@@ -11,5 +12,9 @@ public class PlayerMovement : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	
+		if (Input.GetKeyDown ("space")) {
+			GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpForce));
+			//transform.Translate(new Vector3 (0, 5, 0));
+		}
 	}
 }
